23034ad18ccf36a83dd0617dbf1d420e6ee540c0
Tu Nombre <juanpalbam@gmail.com>
2024-06-03 13:00:35 -0500

    carrito empieza

--- store-app/app/category/[filter]/page.jsx
+++ store-app/app/category/[filter]/page.jsx
@@ -14,7 +14,9 @@ export default async function page({params}) {
     const categoryProducts= await loadFilter(params.filter);
     console.log("llega",categoryProducts)
     // categoria a la que me dirijo
-    let titleCategory = params.filter
+
+    // decodeURIComponent: es un metodo que decodifica una url y la converite a texto normal (Javascript)
+    let titleCategory = decodeURIComponent("Categoria: "+params.filter)
     return (
         <div>
             <h2 className="title">{titleCategory}</h2>
--- store-app/components/AllProducts.jsx
+++ store-app/components/AllProducts.jsx
@@ -3,6 +3,10 @@
 import Link from "next/link"
 import "@/app/styles/styleMain.css"
 
+function addShopCar() {
+    alert("Hola mundo");
+}
+
 function AllProducts({ product }) {
     return (
         <section className="product product__main">
@@ -12,7 +16,7 @@ function AllProducts({ product }) {
                 <img className="product__img" src={product.image} alt="Image"/>
                 <p><strong>Precio Del Producto: </strong>{"$"+product.price}</p>
             </Link>
-            <button className="product__btn" onClick={() => { alert("aaaa") }}>Comprar</button>
+            <button className="product__btn" onClick={addShopCar} >Comprar</button>
         </section>
     )
 }
